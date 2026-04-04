f6360124ee98d00da1f1630e3094445c29907b82
student-shivam <shivamyadav7706876498@gmail.com>
2026-04-04 21:47:39 +0530

    change for deploye

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,8 +6,8 @@ const APP_NAME = process.env.APP_NAME || 'RavindraNexus';
 
 // Generate JWT Token
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE,
+  return jwt.sign({ id }, process.env.JWT_SECRET || 'secret_key', {
+    expiresIn: process.env.JWT_EXPIRE || '30d',
   });
 };
 
